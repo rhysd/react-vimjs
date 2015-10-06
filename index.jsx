7301edc09b1c00848fdb8faf8542ee33ce814397
rhysd <lin90162@yahoo.co.jp>
2015-10-06 19:26:58 +0900

    update example following 983495f72147185c83638112ab1a38ddd20c8c07

--- index.jsx
+++ index.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
-import Vim from 'react-vimjs';
+import React from 'react'
+import Vim from 'react-vimjs'
+
+const props = {
+    vimjsPath: 'node_modules/react-vimjs/dist/vim.js',
+    memPath: 'node_modules/react-vimjs/dist/vim.js.mem'
+};
 
 React.render(
-    <Vim vimjsPath="node_modules/react-vimjs/dist/vim.js" memPath="node_modules/react-vimjs/dist/vim.js.mem"/>,
+    <Vim {...props}>
+        <h1 className="loading"><i className="fa fa-spinner fa-pulse" /> Now Loading...</h1>
+    </Vim>,
     document.querySelector('.main')
 );
