11fc99c6ba40fb06bb50f775abce785891717177
rhysd <lin90162@yahoo.co.jp>
2015-10-07 17:53:55 +0900

    add live markdown preview demo using marked

--- example/index.jsx
+++ example/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Vim from 'react-vimjs'
+import marked from 'marked'
 
 global.ReactVimJSExample = {};
 
@@ -18,10 +19,13 @@ class VimMarkdown extends React.Component {
     }
 
     getVimrc() {
-        return `function s:executeCallBack()
-" TODO: Execute only when filetype is markdown
-let buf = join(map(getline(1, '$'), 'escape(v:val, "\\\\!''")'), '\\n')
-execute "!ReactVimJSExample.callback('" . buf . "')"
+        return `autocmd FileType *.md setlocal ft=markdown
+function s:executeCallBack()
+  if &ft !==# 'markdown'
+    return
+  endif
+  let buf = join(map(getline(1, '$'), 'escape(v:val, "\\\\!''")'), '\\n')
+  execute "!ReactVimJSExample.callback('" . buf . "')"
 endfunction
 autocmd TextChanged * call <SID>executeCallBack()`;
     }
@@ -30,7 +34,8 @@ autocmd TextChanged * call <SID>executeCallBack()`;
         const props = {
             vimjsPath: 'node_modules/react-vimjs/dist/vim.js',
             memPath: 'node_modules/react-vimjs/dist/vim.js.mem',
-            vimrc: this.getVimrc()
+            vimrc: this.getVimrc(),
+            args: ['test.md'],
         };
 
         return (
@@ -40,8 +45,8 @@ autocmd TextChanged * call <SID>executeCallBack()`;
                         <h1 className="loading"><i className="fa fa-spinner fa-pulse" /> Now Loading...</h1>
                     </Vim>
                 </div>
-                <div className="preview">
-                    {this.state.buffer}
+                <div className="preview markdown-body">
+                    <span dangerouslySetInnerHTML={{__html: marked(this.state.buffer)}}/>
                 </div>
             </div>
         );
